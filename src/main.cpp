eb52cd4186dde44c9fde8e084dbf0d1edce5893c
Matthieu Riolo <matthieu.riolo@googlemail.com>
2019-06-05 12:57:10 +0200

    remove unneeded debug prints

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,6 @@ int main(int argc, char **argv) {
 
 	for(int i = 1; i < argc; i++) {
 		if(res.parse(argv[i]) == 0) {
-			std::cout << "successful - read in ";
-			std::cout << res.module.instructions.size();
-			std::cout << " instructions\n";
-
 			res.module.execute();
 		}else {
 			std::cout << "failure\n";
