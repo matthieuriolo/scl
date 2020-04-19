048c979d62f0f5ac060e2aa02fcd3c012c0609ef
Matthieu Riolo <matthieu.riolo@googlemail.com>
2020-04-19 19:33:22 +0200

    new subcommand system with a help text

--- src/main.cpp
+++ src/main.cpp
@@ -11,42 +11,69 @@
 
 using namespace antlr4;
 
+void help() {
+	std::cout << "SCL - System Control Language\n";
+	std::cout << "subcommands:\n";
+	std::cout << "  help     shows this help text\n";
+	std::cout << "  run      executes the given scl files\n";
+	std::cout << "  ast      displays the ast of the given scl files\n";
+	std::cout << "  parse    displays the parser trace given scl files\n";
+}
+
 int main(int argc, char **argv) {
-	bool displayAST = false;
-	bool displayTrace = false;
+	if(argc < 2) {
+		help();
+		return 0;
+	}
 
-	for(int i = 1; i < argc; i++) {
-		if(strcasecmp(argv[i], "--ast") == 0) {
+	if(strcasecmp(argv[1], "help") == 0) {
+		help();
+		return 0;
+	}else {
+		bool displayAST = false;
+		bool displayTrace = false;
+		
+		if(strcasecmp(argv[1], "ast") == 0) {
 			displayAST = true;
-			continue;
-		}else if(strcasecmp(argv[i], "--trace") == 0) {
+		}else if(strcasecmp(argv[1], "parse") == 0) {
 			displayTrace = true;
-			continue;
+		}else if(strcasecmp(argv[1], "run") != 0) {
+			std::cout << "Unknown command '" << argv[1] << "'\n";
+			help();
+			return 1;
 		}
+		
+		for(int i = 2; i < argc; i++) {
+			try {
+				std::ifstream stream;
+				stream.open(argv[i]);
 
-		try {
-			std::ifstream stream;
-			stream.open(argv[i]);
+				ANTLRInputStream input(stream);
+				sclLexer lexer(&input);
 
-			ANTLRInputStream input(stream);
-			sclLexer lexer(&input);
-
-			CommonTokenStream tokens(&lexer);
-			sclParser parser(&tokens);
-			parser.setTrace(displayTrace);
-
-			sclParser::ModuleContext* tree = parser.module();
-			if(!displayTrace) {
-				SCL::ASTVisitor visitor;
-				SCL::Module* module = visitor.visitModule(tree);
-				if(displayAST) {
-					module->printAST();
-				}else {
-					module->launch();
+				CommonTokenStream tokens(&lexer);
+				sclParser parser(&tokens);
+				parser.setTrace(displayTrace);
+				if(displayTrace) {
+					std::cout << "File: " << argv[i] << "\n";
 				}
+				
+				sclParser::ModuleContext* tree = parser.module();
+				if(!displayTrace) {
+					SCL::ASTVisitor visitor;
+					SCL::Module* module = visitor.visitModule(tree);
+					if(displayAST) {
+						std::cout << "File: " << argv[i] << "\n";
+						module->printAST();
+					}else {
+						module->launch();
+					}
+				}
+			}catch(const std::exception* e) {
+				std::cout << "Standard exception: " << e->what() << std::endl;
 			}
-		}catch(const std::exception* e) {
-			std::cout << "Standard exception: " << e->what() << std::endl;
 		}
 	}
+
+	return 0;
 }
